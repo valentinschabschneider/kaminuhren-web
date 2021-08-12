d4187dbb67c8e35bcc9b585d8291e265640a7013
valentinschabscheider <valentin.schabschneider@gmail.com>
2021-08-13 00:57:45 +0200

    created error component

---
+++ src/components/Error.tsx
@@ -0,0 +1,29 @@
+import Head from 'next/head';
+
+import {
+  Center,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  CloseButton,
+} from '@chakra-ui/react';
+
+const Error = () => {
+  return (
+    <>
+      <Head>
+        <title>Fehler!</title>
+      </Head>
+      <Center height="100vh">
+        <Alert status="error">
+          <AlertIcon />
+          <AlertTitle mr={2}>Fehler!</AlertTitle>
+          <AlertDescription>Es ist ein Fehler aufgetreten.</AlertDescription>
+        </Alert>
+      </Center>
+    </>
+  );
+};
+
+export default Error;
