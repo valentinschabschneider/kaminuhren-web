70c4cd499a91214d4eb44998aad0babc1f002d7c
valentinschabscheider <valentin.schabschneider@gmail.com>
2021-08-13 19:23:47 +0200

    styled clock detail component

--- src/components/ClockDetail.tsx
+++ src/components/ClockDetail.tsx
@@ -1,8 +1,23 @@
 import Head from 'next/head';
 
+import {
+  Flex,
+  Box,
+  Text,
+  Spacer,
+  Image,
+  Popover,
+  PopoverTrigger,
+  IconButton,
+  PopoverContent,
+  PopoverArrow,
+  PopoverBody,
+} from '@chakra-ui/react';
+
 import { getClockTypeByLink } from '@/utils/misc';
 import { IMAGE_NOT_FOUND_URL } from '@/constants';
 import { Clock } from '@/types';
+import { QRCodeIcon } from '@/components/Icons';
 
 interface ClockDetailProps {
   clock: Clock;
@@ -16,26 +31,37 @@ const ClockDetail: React.FC<ClockDetailProps> = ({ clock }) => {
           {getClockTypeByLink(clock.type)?.singularDisplayName} #{clock.id}
         </title>
       </Head>
-      <p>{clock?.description}</p>
-      {clock?.qrCodeUrl !== undefined ? (
-        <img
-          src={clock!.qrCodeUrl}
-          onError={(e) => {
-            const image = e.target as HTMLImageElement;
-            image.src = IMAGE_NOT_FOUND_URL;
-          }}
-        />
-      ) : (
-        <p>missing qr code</p>
-      )}
+      <Flex>
+        <Box pb="2" pr="2">
+          {clock.description ? (
+            clock.description.map((line) => <Text>{line}</Text>)
+          ) : (
+            <Text>{clock.description || 'Keine Beschreibung verfügbar.'}</Text>
+          )}
+        </Box>
+        <Spacer />
+        <Popover>
+          <PopoverTrigger>
+            <IconButton
+              isDisabled={!clock.qrCodeUrl}
+              aria-label="QR-Code"
+              icon={<QRCodeIcon />}
+            />
+          </PopoverTrigger>
+          <PopoverContent backgroundColor="white">
+            <PopoverArrow />
+            <PopoverBody>
+              <Image src={clock.qrCodeUrl} fallbackSrc={IMAGE_NOT_FOUND_URL} />
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
+      </Flex>
       {clock?.imageUrls !== undefined && clock?.imageUrls.length > 0 ? (
         clock?.imageUrls.map((url) => (
-          <img
+          <Image
             src={url}
-            onError={(e) => {
-              const image = e.target as HTMLImageElement;
-              image.src = IMAGE_NOT_FOUND_URL;
-            }}
+            fallbackSrc={IMAGE_NOT_FOUND_URL}
+            borderRadius="md"
           />
         ))
       ) : (
