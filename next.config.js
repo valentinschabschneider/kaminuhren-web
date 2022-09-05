e1c07ff5e1cadbffe948c7fda6247e3969ca9272
Valentin Schabschneider <valentin.schabschneider@gmail.com>
2022-09-05 13:45:49 +0200

    Add maintenance mode

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === "true"
+        ? { source: "/((?!maintenance).*)", destination: "/maintenance.html", permanent: false }
+        : null,
+    ].filter(Boolean);
+  }
 };
